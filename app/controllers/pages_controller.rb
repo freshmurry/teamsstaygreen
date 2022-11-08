7cf49463926e84a7fb6d7ce828db2c90b12a8ab9
Ubuntu <ubuntu@ip-172-31-31-224.ec2.internal>
2022-11-08 06:04:50 +0000

    Trying to get recepit and download page to show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @product = Product.find_by_sku("eGuide")
+    # @product = Product.find_by_sku("eGuide")
+    @product = Product.find(params[:product_id])
   end
 end
