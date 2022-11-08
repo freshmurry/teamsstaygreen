80be69b22015ff600acd2f137b27f5e5cdeac1bf
Lawrence <lawrencemurry@yahoo.com>
2022-11-08 10:32:19 +0000

    Trying to get receipt and download page to show

--- config/environment.rb
+++ config/environment.rb
@@ -4,28 +4,22 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sendgrid.net',
-  :port => '587',
-  :authentication => :plain,
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'https://teamsstaygreen.herokuapp.com',
-  :enable_startstls_auto => true
-}
-
-config.action_mailer.raise_delivery_errors = true
-  
-  config.action_mailer.delivery_method = :smtp
-  
-  config.action_mailer.default_url_options = { host: 'https://www.teamsstaygreen.herokuapp.com' }
+# ActionMailer::Base.smtp_settings = {
+#   :address => 'smtp.sendgrid.net',
+#   :port => '587',
+#   :authentication => :plain,
+#   :user_name => ENV['SENDGRID_USERNAME'],
+#   :password => ENV['SENDGRID_PASSWORD'],
+#   :domain => 'https://teamsstaygreen.herokuapp.com',
+#   :enable_startstls_auto => true
+# }
   
   ActionMailer::Base.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
-      :port           => '587',
-      :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => 'https://teamsstaygreen.herokuapp.com',
-      :enable_starttls_auto => true
-    }
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'https://teamsstaygreen.herokuapp.com',
+    :enable_starttls_auto => true
+  }
