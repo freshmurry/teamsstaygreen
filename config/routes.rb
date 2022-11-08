2e03999f2be6c949ccfca4ea78c9084b234e1199
Lawrence <lawrencemurry@yahoo.com>
2022-11-08 06:54:58 +0000

    Updated Code

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   resources :purchases, only: [:show]
   
   # Route for showing downloads
-  resources :downloads, only: [:show]
+  # resources :downloads, only: [:show]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
