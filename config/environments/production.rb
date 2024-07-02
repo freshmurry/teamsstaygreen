3fe61040dd184ebbee16682018f57cbfb51b33c0
Lawrence <lawrencemurry@yahoo.com>
2024-07-02 00:47:18 -0500

    Adjust Gemfile for compatibility

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,17 +83,17 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   
-  # config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   
-  # config.action_mailer.default_url_options = { host: 'https://teamsstaygreen.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://teamsstaygreen.herokuapp.com' }
   
-  # ActionMailer::Base.smtp_settings = {
-  #   :address        => 'smtp.sendgrid.net',
-  #   :port           => '587',
-  #   :authentication => :plain,
-  #   :user_name      => ENV['SENDGRID_USERNAME'],
-  #   :password       => ENV['SENDGRID_PASSWORD'],
-  #   :domain         => 'https://teamsstaygreen.herokuapp.com',
-  #   :enable_starttls_auto => true
-  # }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'https://teamsstaygreen.herokuapp.com',
+    :enable_starttls_auto => true
+  }
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,13 @@
+# Rails.configuration.stripe = {
+#   :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+#   :secret_key      => ENV['STRIPE_SECRET_KEY']
+# }
+
+# Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
 Rails.configuration.stripe = {
-  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-  :secret_key      => ENV['STRIPE_SECRET_KEY']
+  :publishable_key => ENV['pk_test_51LrXmTKRkcJi277FnvAPSfurJdJkx1hXcclRcqNBYLoNLU4cTCwUS2RhOJZ4BYsHIprBj84XR2WH5PeiVauhpM5r00ILTyqILx'],
+  :secret_key      => ENV['sk_test_51LrXmTKRkcJi277FWbRCnNKNwTrAcExZJjGsyYEgGGIvVRyzk0UHuqqko0lzcxgO1EojqiXWeoZFPwXuHRD4sJKP00jUhJ6hsf']
 }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = Rails.configuration.stripe[:sk_test_51LrXmTKRkcJi277FWbRCnNKNwTrAcExZJjGsyYEgGGIvVRyzk0UHuqqko0lzcxgO1EojqiXWeoZFPwXuHRD4sJKP00jUhJ6hsf]
