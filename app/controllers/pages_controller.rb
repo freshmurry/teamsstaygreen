bbd53af59e87abe8f640ff13d8dc098eda2a91f3
Lawrence <lawrencemurry@yahoo.com>
2024-07-09 01:12:46 -0500

    Committing local changes before merge/rebase

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def home
+<<<<<<< HEAD
     @product = Product.find_by(sku: "eGuide")
+=======
+>>>>>>> 1fe3fee (Created simple hello world home page)
   end
 end
